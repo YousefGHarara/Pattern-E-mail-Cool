9775c50c12408a66a59e03085d8bf17e8195f5c7
yousef harara <hrarty37sika2@gmail.com>
2023-08-24 21:34:36 +0300

    FirePWA

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import "./App.css";
 
 function App() {
@@ -15,15 +16,42 @@ function App() {
   }, [mail]);
 
   return (
+    
     <div className="App">
+
+      <Helmet>
+        <title>Hello From Helmet</title>
+        <meta name="description" content="Hello Negga" />
+      </Helmet>
+
+      <div className="style-name">
+        <h2 className="name">Yousef Ghazy Harara</h2>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="200"
+          height="27"
+          viewBox="0 0 345 27"
+          fill="none"
+        >
+          <path
+            d="M1 25C79.6042 7.64366 258.25 -16.6552 344 25"
+            stroke="#0F7771"
+            stroke-width="3"
+          />
+        </svg>
+      </div>
       <h3>Check Pattern E-mail</h3>
-      <input type="text" onChange={(e) => setMail(e.target.value)} style={isValid ? {borderColor : 'green'} : {borderColor : "red"} } />
-      {isValid && 
-      <>
-        <h3 className="msg-success">Is Valid</h3>
-        <button className="btn-success">Submit</button>
-      </>
-      }
+      <input
+        type="text"
+        onChange={(e) => setMail(e.target.value)}
+        style={isValid ? { borderColor: "green" } : { borderColor: "red" }}
+      />
+      {isValid && (
+        <>
+          <h3 className="msg-success">Is Valid</h3>
+          <button className="btn-success">Submit</button>
+        </>
+      )}
       {!isValid && <h3 className="msg-error">Is Not Valid</h3>}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
+
+
+serviceWorkerRegistration.register();
